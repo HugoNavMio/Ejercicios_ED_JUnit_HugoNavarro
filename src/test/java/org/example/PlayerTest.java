739191d0ad65a800f74abc9846b1dc2756c04c80
Hugo_Navarro <tehspartanhugo@outlook.com>
2026-03-11 20:14:59 +0100

    Archivo PlayerTest.java modificado

--- src/test/java/org/example/PlayerTest.java
+++ src/test/java/org/example/PlayerTest.java
@@ -1,27 +1,51 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    private Player player;
+
+    @BeforeAll
+    static void iniciarSuite() {
+        System.out.println("¡Comienzan los tests!");
+    }
+
+    @BeforeEach
+    void setup() {
+        final String nombre = "Antonio";
+        final int vida = 50;
+        final int ataque = 25;
+        player = new Player(nombre, vida, ataque);
+    }
+
     @Test
     void curarSumaVida() {
-        Player player = new Player("Marcheli", 50, 25);
         player.curar(10);
         assertEquals(60, player.getVida());
     }
+
     @Test
     void curarNoSuperaMaximo() {
-        Player player2 = new Player("Yandel", 95, 25);
-        player2.curar(10);
-        assertEquals(100, player2.getVida());
+        player.curar(100);
+        assertEquals(100, player.getVida());
     }
+
     @Test
     void curarAUnMuertoNoFunciona() {
-        Player player3 = new Player("Terrio", 50, 30);
-        player3.recibirDanyo(50);
-        player3.curar(25);
-        assertEquals(0, player3.getVida());
+        player.recibirDanyo(50);
+        player.curar(25);
+        assertEquals(0, player.getVida());
+    }
+
+    @AfterEach
+    void tearDown() {
+        player = null;
+    }
+
+    @AfterAll
+    static void finalizarSuite() {
+        System.out.println("¡Finalizan los tests!");
     }
 }
